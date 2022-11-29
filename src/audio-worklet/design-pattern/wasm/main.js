cc3bb3155958ab1ca396ee6c5a92997a3156daa0
Noah Cohn <noahc42@gmail.com>
2022-11-28 20:05:15 -0800

    Update main.js

--- src/audio-worklet/design-pattern/wasm/main.js
+++ src/audio-worklet/design-pattern/wasm/main.js
@@ -5,22 +5,20 @@
 const audioContext = new AudioContext();
 
 const startAudio = async (context) => {
-  await context.audioWorklet.addModule('wasm-worklet-processor.js');
-  const oscillator = new OscillatorNode(context);
-  const bypasser = new AudioWorkletNode(context, 'wasm-worklet-processor');
-  oscillator.connect(bypasser).connect(context.destination);
-  oscillator.start();
+await context.audioWorklet.addModule('wasm-worklet-processor.js');
+const oscillator = new OscillatorNode(context);
+const bypasser = new AudioWorkletNode(context, 'wasm-worklet-processor');
+oscillator.connect(bypasser).connect(context.destination);
+oscillator.start();
 };
 
-// A simple onLoad handler. It also handles user gesture to unlock the audio
-// playback.
 window.addEventListener('load', async () => {
-  const buttonEl = document.getElementById('button-start');
-  buttonEl.disabled = false;
-  buttonEl.addEventListener('click', async () => {
-    await startAudio(audioContext);
-    audioContext.resume();
-    buttonEl.disabled = true;
-    buttonEl.textContent = 'Playing...';
-  }, false);
+const buttonEl = document.getElementById('button-start');
+buttonEl.disabled = false;
+buttonEl.addEventListener('click', async () => {
+await startAudio(audioContext);
+audioContext.resume();
+buttonEl.disabled = true;
+buttonEl.textContent = 'Playing...';
+}, false);
 });
